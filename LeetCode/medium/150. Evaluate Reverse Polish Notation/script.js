4bdda62a52f21c985c2c776d2102fd6e2f5bfc0a
TwiiiXeR <36134927+AshrafSamir@users.noreply.github.com>
2023-02-12 10:36:07 +0200

    Problem Solving: evaluate reverse polish notation

---
+++ LeetCode/medium/150. Evaluate Reverse Polish Notation/script.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  let stack = [];
+  let operators = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => (a / b) | 0,
+  };
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i] in operators) {
+      let b = stack.pop();
+      let a = stack.pop();
+      stack.push(operators[tokens[i]](a, b));
+    } else {
+      stack.push(+tokens[i]);
+    }
+  }
+  return stack.pop();
+};
+
+//Test cases
+console.log(evalRPN(["2", "1", "+", "3", "*"])); // 9
+console.log(evalRPN(["4", "13", "5", "/", "+"])); // 6
+console.log(
+  evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"])
+); // 22
